413e8dd263cb1b3a91476fc995f483b0ee88926d
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2023-10-17 16:16:01 +0200

    Added keyboard interrupt handler

--- example/3_led_blink.py
+++ example/3_led_blink.py
@@ -12,14 +12,21 @@ GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 
-while True :
-    GPIO.output(led1, GPIO.HIGH)
-    GPIO.output(led2, GPIO.HIGH)
-    GPIO.output(led3, GPIO.HIGH)
-    time.sleep(1)
+try:
+    while True :
+        GPIO.output(led1, GPIO.HIGH)
+        GPIO.output(led2, GPIO.HIGH)
+        GPIO.output(led3, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(led1, GPIO.LOW)
+        GPIO.output(led2, GPIO.LOW)
+        GPIO.output(led3, GPIO.LOW)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+finally:
     GPIO.output(led1, GPIO.LOW)
     GPIO.output(led2, GPIO.LOW)
     GPIO.output(led3, GPIO.LOW)
-    time.sleep(1)
-
-GPIO.cleanup()
+    GPIO.cleanup()
