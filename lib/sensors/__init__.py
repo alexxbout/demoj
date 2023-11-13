d1c36173231e4dac908e70877827364959939cb9
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2023-11-13 11:52:52 +0100

    Made a wattmeter class not tested yet
    also added example from DFRobot

---
+++ lib/sensors/__init__.py
@@ -0,0 +1,1 @@
+# mark this directory as a package directory
---
+++ src/main/python/sensors_and_disp/WattmeterTimeout.py
@@ -0,0 +1,7 @@
+class WattmeterTimeout(Exception):
+    """
+    When a timeout error occurs during the wattmeter execution
+    """
+    def __init__(self, message="A timeout occured during the wattmeter execution"):
+        self.message = message
+        super().__init__(self.message)
