d95f2dc1d01748bce65448846b85ac08e5c10c0d
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2023-12-06 15:49:09 +0100

    add an home-made example for strip leds

---
+++ example/demotech/leds/strip_led_test2.py
@@ -0,0 +1,63 @@
+# DEMOTECH Example for using strip leds with a gradiant color
+
+from rpi_ws281x import PixelStrip, Color
+import time
+
+# LED strip configuration:
+LED_COUNT = 30       # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 128  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+BEGIN_COLOR = Color(255, 0, 0)
+ENDING_COLOR = Color(0, 0, 255)
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
+def wipe(strip: PixelStrip, color: Color):
+    pixels = strip.numPixels()
+    for i in range(pixels):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(0.100) # For something animated
+
+def wipeRev(strip: PixelStrip, color: Color):
+    pixels = strip.numPixels()
+    for i in range(pixels):
+        strip.setPixelColor(pixels-i-1, color)
+        strip.show()
+        time.sleep(0.100) # For something animated
+
+def degrade(i: int, max: int, begin: Color, end: Color): 
+    percent = float(i / max)
+    red = begin.r() - (end.r() * percent)
+    green = begin.r() - (end.r() * percent)
+    blue = begin.r() - (end.r() * percent)
+    return Color(red, green, blue)
+
+def degradeAll(strip: PixelStrip):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, degrade(i, strip.numPixels(), BEGIN_COLOR, ENDING_COLOR))
+        strip.show()
+        time.sleep(0.100)
+
+def degradeAllRev(strip: PixelStrip):
+    pixels = strip.numPixels()
+    for i in range(pixels):
+        strip.setPixelColor(pixels-i-1, degrade(i, strip.numPixels(), BEGIN_COLOR, ENDING_COLOR))
+        strip.show()
+        time.sleep(0.100)
+
+try:
+    while True:
+        degradeAll(strip)
+        wipe(strip)
+        degradeAllRev(strip)
+        wipeRev(strip)
+
+except KeyboardInterrupt:
+    wipe(strip, Color(0, 0, 0))
+
+
