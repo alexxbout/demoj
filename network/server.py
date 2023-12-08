c275d3d0a35061383a41d403849d613679d6bf41
Alexandre <alexandre.boutinaud@etudiant.univ-rennes1.fr>
2023-12-08 21:32:28 +0000

    added sync with param values

--- network/server.py
+++ network/server.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-config = "scripts/config.json"
+config = "network/config.json"
 
 # ! Route to receive data
 @app.route('/receive_data', methods=['POST'])
@@ -28,33 +28,30 @@ def get_config():
         print(f"Error reading config file: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-# ! Route to toggle module's parameters
+# ! Route to update parameter status and value
 @app.route('/modules/<module>/params/<id_param>', methods=['POST'])
-def set_parameter_state(module, id_param):
+def update_parameter(module, id_param):
     try:
         data = request.get_json()
-        is_active = data.get('isActive', False)
+        is_active = data.get('isActive')
+        value = data.get('value')
 
-        with open(config, 'r') as config_file:
+        with open(config, 'r+') as config_file:
             config_data = json.load(config_file)
 
         if 'modules' in config_data and module in config_data['modules']:
-            print(f"Module {module} found")
             device = config_data['modules'][module]
             if 'parameters' in device:
-                print(f"Parameters found for module {module}")
                 for param in device['parameters']:
-                    print(f"Checking parameter {param['id']}")
-                    print(id_param)
                     if int(param['id']) == int(id_param):
-                        print(f"Parameter {id_param} found")
-                        print(f"Setting parameter {id_param} to {is_active}")
-                        param['isActive'] = is_active
+                        if is_active is not None:
+                            param['isActive'] = is_active
+                        if value is not None:
+                            param['value'] = value
                         # TODO : toggle parameter on module
 
-        with open(config, 'w+') as config_file:
-            config_file.write(json.dumps(config_data, indent=2))
-            config_file.close()
+        with open(config, 'w') as config_file:
+            json.dump(config_data, config_file, indent=2)
 
         return jsonify({"message": "Parameter toggled successfully"})
     except Exception as e:
