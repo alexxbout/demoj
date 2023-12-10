b2d340f4a9fd2b08c9899f6eaf817c1e25d86b16
alexxbout <boutinaud.pro@gmail.com>
2023-12-10 01:45:43 +0100

    added restart and stop actions

--- demoj-app/src/views/Scenarios.vue
+++ demoj-app/src/views/Scenarios.vue
@@ -13,21 +13,15 @@
                 </ion-toolbar>
             </ion-header>
 
-            <ion-button color="secondary" class="ion-padding" expand="block">Voir les scénarios en cours</ion-button>
-
             <scenario-card :data="sc[0]" />
-
-            <scenario-card :data="sc[1]" />
-
-            <scenario-card :data="sc[2]" />
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
 import ScenarioCard from "@/components/ScenarioCard.vue";
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/vue";
-import { calculator, image, reader } from "ionicons/icons";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/vue";
+import { calculator } from "ionicons/icons";
 import { ref } from "vue";
 
 const sc = ref([
@@ -37,20 +31,6 @@ const sc = ref([
         subtitle: "Terminal",
         icon: calculator,
         description: "Effectuez des calculs avancés, démontrant la puissance de traitement du serveur dans des tâches informatiques complexes.",
-    },
-    {
-        id: 2,
-        title: "Conversion de données",
-        subtitle: "Terminal",
-        icon: reader,
-        description: "Transformez le format des données avec cette fonctionnalité du terminal.",
-    },
-    {
-        id: 3,
-        title: "Upscale d'Image",
-        subtitle: "Terminal",
-        icon: image,
-        description: "Augmentez la résolution des images avec ce scénario performant.",
-    },
+    }
 ]);
 </script>
