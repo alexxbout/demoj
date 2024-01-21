b3efcd6f458404c874ae6f04ce26d952fbdac19b
Alexandre <alexandre.boutinaud@etudiant.univ-rennes1.fr>
2024-01-21 19:22:59 +0000

    fixed bugs

--- module/scripts/server.py
+++ module/scripts/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from const import IP_TERMINAL, IP_NETWORK, IP_SERVER
-from module.scripts.utils import ping, execute_command
+from utils import ping, execute_command
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -11,8 +11,6 @@ import subprocess
 
 app = Flask(__name__)
 CORS(app)
-
-CONFIG_PATH = "/home/network/demoj/module/config/config.json"
 
 RESTART_CMD = ['sudo', 'reboot']
 STOP_CMD = ['sudo', 'shutdown', '-h', 'now']
