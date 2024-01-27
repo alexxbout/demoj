e93baedb4f78a30e0f585179993e27c8fe6a0866
alexxbout <boutinaud.pro@gmail.com>
2024-01-27 22:04:49 +0100

    added client socket test component

--- demoj-app/src/views/debug/Socket.vue
+++ demoj-app/src/views/debug/Socket.vue
@@ -1,10 +1,51 @@
 <template>
-    <ion-button @click="handleClick" expand="block">Tester la socket serveur</ion-button>
+    <ion-button @click="handleConnect" expand="block">Se connecter à la socket serveur</ion-button>
+    <ion-button @click="handleMsg" expand="block">Envoyer un message</ion-button>
+    <ion-button @click="handleClose" expand="block">Fermer la socket</ion-button>
 </template>
 
 <script setup lang="ts">
 import { IonButton } from "@ionic/vue";
+import { Socket, io } from "socket.io-client";
 
-const handleClick = () => {
+let socket: Socket | null = null;
+
+const handleConnect = () => {
+    socket = io("http://192.168.64.101:5000");
+
+    socket.on("connect", () => {
+        console.log("socket connected");
+        // socket?.emit("message", "hellloooooooo");
+    });
+
+    socket.on("message", (data: any) => {
+        console.log(data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("socket closed");
+    });
+
+    socket.on("error", (error: any) => {
+        console.log(error);
+    });
+
+    socket.on("connect_error", (error: any) => {
+        console.log(error);
+    });
+
+    socket.on("connect_timeout", (error: any) => {
+        console.log(error);
+    });
+};
+
+const handleClose = () => {
+    // On Chrome, this will cause an Invalid frame header error, but it's fine
+    
+    if (socket && socket.connected) socket.disconnect();
+};
+
+const handleMsg = () => {
+    socket?.emit("message", "hellloooooooo");
 };
 </script>
--- demoj-app/vite.config.ts
+++ demoj-app/vite.config.ts
@@ -1,24 +1,16 @@
-import legacy from '@vitejs/plugin-legacy';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy(),
-    VitePWA({ registerType: 'autoUpdate' })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue(), legacy(), VitePWA({ registerType: "autoUpdate" })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  },
-  base: '/',
-})
+    base: "/",
+});
