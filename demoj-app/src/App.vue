4ae47cf09438ffea28a1fed72c651313012bb449
alexxbout <boutinaud.pro@gmail.com>
2024-01-28 16:10:47 +0100

    added red indicators in tabs

--- demoj-app/src/App.vue
+++ demoj-app/src/App.vue
@@ -9,12 +9,12 @@ import { IonApp, IonRouterOutlet } from "@ionic/vue";
 import { inject, onMounted } from "vue";
 import { CustomSocket } from "./services/CustomSocket";
 
-const socket: CustomSocket | undefined = inject("socket");
+const socket = inject("socket") as CustomSocket;
 
 onMounted(() => {
     console.log("Hello DémoJ Connect");
     console.log("Starting socket connection");
-    if (socket) socket.connect();
+    socket.connect();
 
     if (!localStorage.getItem("mode")) localStorage.setItem("mode", "client");
 });
--- demoj-app/src/views/Tabs.vue
+++ demoj-app/src/views/Tabs.vue
@@ -11,16 +11,22 @@
                 <ion-tab-button tab="terminal" href="/tabs/terminal">
                     <ion-icon aria-hidden="true" :icon="desktop" />
                     <ion-label>Terminal</ion-label>
+
+                    <div v-show="!status.terminal" style="background-color: #eb445a; position: absolute; top: 2px; right: 16px; width: 17px; height: 17px; border-radius: 9999px" />
                 </ion-tab-button>
 
                 <ion-tab-button tab="network" href="/tabs/network">
                     <ion-icon aria-hidden="true" :icon="wifi" />
                     <ion-label>Réseau</ion-label>
+
+                    <div v-show="!status.network" style="background-color: #eb445a; position: absolute; top: 2px; right: 16px; width: 17px; height: 17px; border-radius: 9999px" />
                 </ion-tab-button>
 
                 <ion-tab-button tab="server" href="/tabs/server">
                     <ion-icon aria-hidden="true" :icon="terminal" />
                     <ion-label>Serveur</ion-label>
+
+                    <div v-show="!status.server" style="background-color: #eb445a; position: absolute; top: 2px; right: 16px; width: 17px; height: 17px; border-radius: 9999px" />
                 </ion-tab-button>
 
                 <ion-tab-button v-show="debugModeEnabled" tab="debug" href="/tabs/debug">
@@ -33,20 +39,33 @@
 </template>
 
 <script setup lang="ts">
+import { CustomSocket } from "@/services/CustomSocket";
 import { IonIcon, IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from "@ionic/vue";
 import { desktop, hardwareChip, layers, terminal, wifi } from "ionicons/icons";
-import { onMounted, ref } from "vue";
+import { computed, inject, onMounted, ref } from "vue";
+
+const socket = inject("socket") as CustomSocket;
+const config = socket.getConfig();
+
+const status = computed(() => {
+    return {
+        terminal: config.value?.modules.terminal.isConnected ?? false,
+        network: config.value?.modules.network.isConnected ?? false,
+        server: config.value?.modules.server.isConnected ?? false,
+    };
+});
 
 const mode = ref<"client" | "operator">("client");
 const debugModeEnabled = ref(false);
 
 onMounted(() => {
-    if (localStorage.getItem("debugMode") === "true") {
-        debugModeEnabled.value = true;
-    }
-
-    if (localStorage.getItem("mode") === "operator") {
-        mode.value = "operator";
-    }
+    if (localStorage.getItem("debugMode") === "true") debugModeEnabled.value = true;
+    if (localStorage.getItem("mode") === "operator") mode.value = "operator";
 });
 </script>
+
+<style scoped>
+ion-tab-button {
+    position: relative;
+}
+</style>
