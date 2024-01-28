ce2ea24d03a07e436b0f315299f32aaed83a7d7b
Alexandre <alexandre.boutinaud@etudiant.univ-rennes1.fr>
2024-01-28 10:48:51 +0000

    added init socket code and room

--- module/scripts/server.py
+++ module/scripts/server.py
@@ -1,65 +1,69 @@
 #!/usr/bin/env python3
-
-from const import IP_TERMINAL, IP_NETWORK, IP_SERVER
-from utils import ping, execute_command
-
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
 import json
 import requests
+from const import IP_TERMINAL, IP_NETWORK, IP_SERVER
+from utils import ping, execute_command
+import threading
+
+from flask import Flask, jsonify, request
+from flask_socketio import SocketIO, join_room, leave_room
+from flask_cors import CORS
 
 app = Flask(__name__)
+sio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
 CONFIG_PATH = "/home/network/demoj/module/config/config.json"
+SERVER_PORT = 5000
 
-RESTART_CMD = ['sudo', 'reboot']
-STOP_CMD = ['sudo', 'shutdown', '-h', 'now']
+RESTART_CMD = ["sudo", "reboot"]
+STOP_CMD = ["sudo", "shutdown", "-h", "now"]
 
+# ! DEPRECATED
 def geHttpAddress(ip):
-    return 'http://' + ip + ':5000'
+    return ("http://{}:{}".format(ip, SERVER_PORT))
 
-@app.route('/')
+#################################################################
+# DémoJ Connect
+#################################################################
+
+@app.route("/")
 def index():
     return jsonify({"message": "Server is running"})
 
-#################################################################
-# DémoJ Connect routes
-#################################################################
-@app.route('/receive_data', methods=['POST'])
+@app.route("/receive_data", methods=["POST"])
 def receive_data():
     data = request.get_json()
     print("Received data:", data)
 
     return jsonify({"message": "Data received successfully"})
 
-@app.route('/restart/<module>', methods=['GET'])
+@app.route("/restart/<module>", methods=["GET"])
 def restart_module(module):
-    if module == 'terminal':
-        return jsonify(requests.get(geHttpAddress(IP_TERMINAL) + '/restart').status_code == 200)
-    elif module == 'server':
-        return jsonify(requests.get(geHttpAddress(IP_SERVER) + '/restart').status_code == 200)
-    elif module == 'network':
+    if module == "terminal":
+        return jsonify(requests.get(geHttpAddress(IP_TERMINAL) + "/restart").status_code == 200)
+    elif module == "server":
+        return jsonify(requests.get(geHttpAddress(IP_SERVER) + "/restart").status_code == 200)
+    elif module == "network":
         return jsonify(execute_command(RESTART_CMD));
     else:
         return jsonify({"error": "Invalid module"}), 400
 
-@app.route('/stop/<module>', methods=['GET'])
+@app.route("/stop/<module>", methods=["GET"])
 def stop_module(module):
-    if module == 'terminal':
-        return jsonify(requests.get(geHttpAddress(IP_TERMINAL) + '/stop').status_code == 200)
-    elif module == 'server':
-        return jsonify(requests.get(geHttpAddress(IP_SERVER) + '/stop').status_code == 200)
-    elif module == 'network':
+    if module == "terminal":
+        return jsonify(requests.get(geHttpAddress(IP_TERMINAL) + "/stop").status_code == 200)
+    elif module == "server":
+        return jsonify(requests.get(geHttpAddress(IP_SERVER) + "/stop").status_code == 200)
+    elif module == "network":
         return jsonify(execute_command(STOP_CMD));
     else:
         return jsonify({"error": "Invalid module"}), 400
 
-@app.route('/config', methods=['GET'])
+@app.route("/config", methods=["GET"])
 def get_config():
     try:
-        with open(CONFIG_PATH, 'r') as config_file:
+        with open(CONFIG_PATH, "r") as config_file:
             config_data = json.load(config_file)
             return jsonify(config_data)
     except FileNotFoundError:
@@ -68,28 +72,28 @@ def get_config():
         print(f"Error reading config file: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-@app.route('/modules/<module>/params/<id_param>', methods=['POST'])
+@app.route("/modules/<module>/params/<id_param>", methods=["POST"])
 def update_parameter(module, id_param):
     try:
         data = request.get_json()
-        is_active = data.get('isActive')
-        value = data.get('value')
+        is_active = data.get("isActive")
+        value = data.get("value")
 
-        with open(CONFIG_PATH, 'r+') as config_file:
+        with open(CONFIG_PATH, "r+") as config_file:
             config_data = json.load(config_file)
 
-        if 'modules' in config_data and module in config_data['modules']:
-            device = config_data['modules'][module]
-            if 'parameters' in device:
-                for param in device['parameters']:
-                    if int(param['id']) == int(id_param):
+        if "modules" in config_data and module in config_data["modules"]:
+            device = config_data["modules"][module]
+            if "parameters" in device:
+                for param in device["parameters"]:
+                    if int(param["id"]) == int(id_param):
                         if is_active is not None:
-                            param['isActive'] = is_active
+                            param["isActive"] = is_active
                         if value is not None:
-                            param['value'] = value
+                            param["value"] = value
                         # TODO : toggle parameter on module
 
-        with open(CONFIG_PATH, 'w') as config_file:
+        with open(CONFIG_PATH, "w") as config_file:
             json.dump(config_data, config_file, indent=2)
 
         return jsonify({"message": "Parameter toggled successfully"})
@@ -97,25 +101,25 @@ def update_parameter(module, id_param):
         print(f"Error toggling parameter: {str(e)}")
         return jsonify({"error": str(e)}), 500
     
-@app.route('/ping/<module>', methods=['GET'])
+@app.route("/ping/<module>", methods=["GET"])
 def ping_module(module):
-    if module == 'terminal':
+    if module == "terminal":
         return jsonify(ping(IP_TERMINAL))
-    elif module == 'server':
+    elif module == "server":
         return jsonify(ping(IP_SERVER))
-    elif module == 'all':
+    elif module == "all":
         return jsonify(ping(IP_TERMINAL) and ping(IP_SERVER))
     else:
         return jsonify({"error": "Invalid module"}), 400
     
-@app.route('/check_status/<module>', methods=['GET'])
+@app.route("/check_status/<module>", methods=["GET"])
 def check_status(module):
     try:
-        if module == 'terminal':
+        if module == "terminal":
             return jsonify(requests.get(geHttpAddress(IP_TERMINAL)).status_code == 200)
-        elif module == 'server':
+        elif module == "server":
             return jsonify(requests.get(geHttpAddress(IP_SERVER)).status_code == 200)
-        elif module == 'network':
+        elif module == "network":
             return jsonify(True)
         else:
             return jsonify({"error": "Invalid module"}), 400
@@ -123,8 +127,34 @@ def check_status(module):
         return jsonify(False), 200
     
 #################################################################
-# Scenarios routes
+# Scenarios
+#################################################################
+    
+# TODO
+    
+#################################################################
+# Socket
 #################################################################
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@sio.on("connect")
+def handle_connect():
+    print("Client connected")
+    print("ip: ", request.remote_addr)
+
+@sio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected")
+
+@sio.on("module_ready")
+def handle_module_ready(data):
+    room = data["module"]
+    print("Module ready:", room)
+    join_room(room)
+    print("Successfully joined room:", room)
+
+#################################################################
+# Main
+#################################################################
+
+if __name__ == "__main__":
+    sio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
--- module/scripts/utils.py
+++ module/scripts/utils.py
@@ -2,8 +2,19 @@
 import os
 import subprocess
 import time
+import json
+from functools import reduce
 
 def execute_command(command):
+    """
+    Execute a command.
+
+    Parameters:
+    - command (list): The command to execute.
+
+    Returns:
+    - True if the command was executed successfully, False otherwise.
+    """
     try:
         subprocess.Popen(command)
         return True
@@ -12,4 +23,67 @@ def execute_command(command):
         return False
 
 def ping(hostname):
+    """
+    Ping a host and return True if it is reachable, False otherwise.
+
+    Parameters:
+    - hostname (str): The hostname to ping.
+
+    Returns:
+    - True if the host is reachable, False otherwise.
+    """
     return os.system("ping -c 1 " + hostname) == 0
+
+def update_json_value(json_data, path, new_value):
+    """
+    Update the value in a JSON object using a specified path.
+
+    Parameters:
+    - json_data (dict): The JSON data to be updated.
+    - path (str): The path to the value to be updated, e.g., "modules.terminal.isConnected".
+    - new_value: The new value to set.
+
+    Returns:
+    - Updated JSON data.
+    """
+    keys = path.split('.')
+    key_path = keys[:-1]
+    last_key = keys[-1]
+
+    # Use functools.reduce to access nested keys
+    nested_data = reduce(lambda d, key: d[key], key_path, json_data)
+
+    # Update the value
+    nested_data[last_key] = new_value
+
+    return json_data
+
+def write_json_to_file(json_data, file_path):
+    """
+    Write the JSON data to a file.
+
+    Parameters:
+    - json_data (dict): The JSON data to be written.
+    - file_path (str): The path to the file.
+    """
+    with open(file_path, 'w') as json_file:
+        json.dump(json_data, json_file, indent=2)
+
+def update_and_write_json(file_path, path, new_value):
+    """
+    Update the value in a JSON file using a specified path and write the updated JSON to the file.
+
+    Parameters:
+    - file_path (str): The path to the JSON file.
+    - path (str): The path to the value to be updated, e.g., "modules.terminal.isConnected".
+    - new_value: The new value to set.
+    """
+    # Read JSON data from file
+    with open(file_path, 'r') as json_file:
+        json_data = json.load(json_file)
+
+    # Update the value at the specified path
+    updated_json_data = update_json_value(json_data, path, new_value)
+
+    # Write the updated data back to the file
+    write_json_to_file(updated_json_data, file_path)
