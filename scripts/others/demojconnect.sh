e2a98798de39fa0206af0c864b5a7236c2c6e0b5
alexxbout <boutinaud.pro@gmail.com>
2024-02-06 09:32:22 +0100

    updated demojconnect.sh

--- scripts/others/demojconnect.sh
+++ scripts/others/demojconnect.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/sudo bash
 # shellcheck shell=bash
 
-# TODO: Add DNS to demoj.fr
-
 echo "Initializing Demoj Connect"
-
-if [ ! -d "$dir" ]; then
-    echo "Directory /home/network/demoj does not exist. Please run repository.sh first"
-    exit 1
-fi
 
 dir="/home/network/demoj"
 
+if [ ! -d "$dir" ]; then
+    echo "Directory $dir does not exist. Please run repository.sh first"
+    exit 1
+fi
+
 cd "$dir" || exit 1
 
-sudo -u network git checkout demojconnect
+sudo -u network git checkout demojconnect || exit 1
 
 cd "demoj-app" || exit 1
 
 if ! command -v node &> /dev/null; then
     echo "Node is not installed"
-
-    apt install nodejs
+    apt install nodejs -y || exit 1
 
     if ! command -v node &> /dev/null; then
         echo "Node installation failed"
@@ -33,8 +30,7 @@ fi
 
 if ! command -v npm &> /dev/null; then
     echo "Npm is not installed"
-
-    apt install npm
+    apt install npm -y || exit 1
 
     if ! command -v npm &> /dev/null; then
         echo "Npm installation failed"
@@ -45,38 +41,34 @@ if ! command -v npm &> /dev/null; then
 fi
 
 echo "Installing dependencies"
-
-npm install
+npm install || exit 1
 
 echo "Building app"
-
-# TODO: Update .env file
-
-ionic build
+ionic build || exit 1
 
 echo "App built"
 echo "Moving app to /home/network/temp"
 
-if [ ! -d "/home/network/temp" ]; then
-    mkdir /home/network/temp
+temp_dir="/home/network/temp"
+
+if [ ! -d "$temp_dir" ]; then
+    mkdir "$temp_dir" || exit 1
 fi
 
-mv /home/network/demoj/demoj-app/demojconnect /home/network/temp
+mv "$dir/demoj-app/demojconnect" "$temp_dir" || exit 1
 
-echo "App moved to /home/network/temp"
+echo "App moved to $temp_dir"
 
 echo "Switching to network branch"
 
 cd "$dir" || exit 1
 
-sudo -u network git checkout network
+sudo -u network git checkout network || exit 1
 
 echo "Moving app to /home/network/demoj/module"
+mv "$temp_dir/demojconnect" "$dir/module" || exit 1
 
-mv /home/network/temp/demojconnect /home/network/demoj/module
-
-echo "App moved to /home/network/demoj/module"
+echo "App moved to $dir/module"
 
 echo "DemoJ Connect initialized"
-
 exit 0
