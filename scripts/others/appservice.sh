edcf62e2d80d3219baba5feb2d1edbf4d39484d4
alexxbout <boutinaud.pro@gmail.com>
2024-02-06 09:33:40 +0100

    updated appservice.sh

--- scripts/others/appservice.sh
+++ scripts/others/appservice.sh
@@ -1,33 +1,48 @@
-#!/usr/bin/sudo bash
+#!/bin/bash
 # shellcheck shell=bash
 
-# args: user
+# Vérification des arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <user>"
+    exit 1
+fi
 
-echo "Initializing app service"
+# Vérification que le script est exécuté en tant que root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root."
+    exit 1
+fi
 
+# Récupération de l'utilisateur
 user=$1
 
+# Vérification de la validité de l'utilisateur
 if [ "$user" != "terminal" ] && [ "$user" != "network" ] && [ "$user" != "server" ]; then
     echo "Invalid user"
     exit 1
 fi
 
-echo "Creating app.service file"
+# Définition des chemins
+venv_path="/home/$user/demoj/venv/bin/python3"
+app_script="/home/$user/demoj/module/scripts/app.py"
+service_file="/etc/systemd/system/app.service"
 
-file="/etc/systemd/system/app.service"
+echo "Initializing app service"
 
+# Création du fichier de service
 {
     echo "[Unit]"
     echo "Description=Start $user app"
     echo ""
     echo "[Service]"
-    echo "ExecStart=/home/$user/demoj/venv/bin/python3 /home/$user/demoj/module/scripts/app.py"
+    echo "ExecStart=$venv_path $app_script"
     echo "Restart=always"
     echo ""
     echo "[Install]"
     echo "WantedBy=multi-user.target"
-} > $file
+} > "$service_file"
 
+# Activation du service
 echo "Enabling app.service"
 systemctl enable app.service
 
