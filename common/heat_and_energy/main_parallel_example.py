df52e1dc663bb954c0c2a365e6ea413c1202c14c
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2024-03-14 19:51:39 +0100

    made a usage example

---
+++ common/heat_and_energy/main_parallel_example.py
@@ -0,0 +1,12 @@
+from leds_process_control import DemoLedsController
+import time
+
+if __name__ == "__main__":
+    leds = DemoLedsController()
+    leds.loading()
+    #animation should run in parallel
+    time.sleep(5)
+    leds.loading_done()
+    leds.demoj()
+    time.sleep(5)
+    leds.end_animation()
