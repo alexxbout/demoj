cdcf1738bfb5f4485484565d27cc6a8832320dc0
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2024-03-29 10:54:48 +0000

    better main

--- common/heat_and_energy/main_parallel.py
+++ common/heat_and_energy/main_parallel.py
@@ -3,13 +3,13 @@ import time
 
 if __name__ == "__main__":
     leds = DemoLedsController()
-    leds.loading()
-    #animation should run in parallel
-    time.sleep(20)
-    leds.loading_done()
     try:
+        leds.loading()
+        #launch flask
+        time.sleep(20)
+        leds.loading_done()
         leds.demoj()
-        #run flask app
+        
         while True:
             time.sleep(20)
     except KeyboardInterrupt:
