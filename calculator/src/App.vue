dc39e1d2cda2c358d8e803e88aeb89bed1d59395
alexxbout <boutinaud.pro@gmail.com>
2024-03-29 09:28:43 +0100

    new version of the calculator

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,35 +1,27 @@
 <template>
-    <div class="flex flex-col justify-end w-screen h-screen overflow-x-hidden">
-        <div ref="field" class="w-screen p-5 overflow-x-auto scroll-smooth scrollbar-hide">
-            <div class="flex items-center justify-end h-24 min-w-full p-5 ml-auto w-max">
-                <span class="text-6xl font-medium text-slate-700">{{ formula }}</span>
+    <div class="flex flex-col justify-between w-screen h-screen overflow-x-hidden p-5">
+        <div class="w-full h-max flex flex-col gap-y-5">
+            <div class="flex items-center justify-between">
+                <button class="text-blue-600 font-medium text-lg w-max">Retour</button>
+
+                <div class="w-10 h-10">
+                    <img v-show="loading" class="w-full h-full aspect-square" src="./assets/spinner.svg" alt="" />
+                </div>
+            </div>
+            <div class="rounded-xl w-full bg-gray-200 flex items-center justify-center overflow-hidden p-1">
+                <button @click="mode = 'client'" :class="mode == 'client' ? 'bg-white text-black' : 'text-gray-500'" class="w-full py-2 px-5 rounded-lg text-center font-medium text-lg">Client</button>
+                <button @click="mode = 'server'" :class="mode == 'server' ? 'bg-white text-black' : 'text-gray-500'" class="w-full py-2 px-5 rounded-lg text-center font-medium text-lg">Serveur</button>
             </div>
         </div>
 
-        <div class="grid grid-cols-4 gap-4 p-5 pt-10">
-            <Key @@click="clear" text="AC" type="action" />
-            <Key @@click="invertSign" text="+/-" type="action" />
-            <Key @@click="append('%')" text="%" type="action" />
-            <Key @@click="append('/')" text="÷" type="operator" />
-
-            <Key @@click="append('7')" text="7" type="number" />
-            <Key @@click="append('8')" text="8" type="number" />
-            <Key @@click="append('9')" text="9" type="number" />
-            <Key @@click="operator('*')" text="x" type="operator" />
-
-            <Key @@click="append('4')" text="4" type="number" />
-            <Key @@click="append('5')" text="5" type="number" />
-            <Key @@click="append('6')" text="6" type="number" />
-            <Key @@click="operator('-')" text="-" type="operator" />
-
-            <Key @@click="append('1')" text="1" type="number" />
-            <Key @@click="append('2')" text="2" type="number" />
-            <Key @@click="append('3')" text="3" type="number" />
-            <Key @@click="operator('+')" text="+" type="operator" />
-
-            <Key @@click="append('0')" text="0" type="number" :span="2" />
-            <Key @@click="append('.')" text="." type="number" />
-            <Key @@click="equal" text="=" type="operator" />
+        <div class="flex flex-col gap-y-5 w-full h-max">
+            <select class="appearance-none w-full h-max rounded-xl bg-slate-200 px-5 py-4 text-lg font-medium" name="scenarioSelect" id="scenarioSelect">
+                <option value="1">Fibonacci</option>
+                <option value="2">Nombre premier</option>
+                <option value="3">Factorielle</option>
+            </select>
+            <input @input="handleValueChange" v-model="valueInput" type="number" class="appearance-none w-full h-max rounded-xl bg-slate-200 px-5 py-4 text-lg font-medium" placeholder="Valeur..." />
+            <button @click="handleExecute" class="bg-blue-600 text-white w-full h-max py-4 rounded-xl font-medium text-lg disabled:grayscale" :disabled="disabled">Exécuter</button>
         </div>
     </div>
 </template>
@@ -37,74 +29,29 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import Key from "./components/Key.vue";
 
 const router = useRouter();
 const route = useRoute();
 
 const mode = ref<"server" | "client">("client");
-const field = ref<HTMLElement | null>(null);
-const formula = ref("");
 
-const clear = () => {
-    formula.value = "";
+const valueInput = ref<string>();
+const disabled = ref<boolean>(true);
+const loading = ref<boolean>(false);
+
+const handleExecute = () => {
+    console.log("Execute");
+
+    loading.value = true;
+
+    setTimeout(() => {
+        loading.value = false;
+    }, 2000);
 };
 
-// const deleteChar = () => {
-//     formula.value = formula.value.slice(0, -1);
-// };
-
-const invertSign = () => {
-    formula.value = formula.value.startsWith("-") ? formula.value.slice(1) : `-${formula.value}`;
+const handleValueChange = () => {
+    disabled.value = valueInput.value === "";
 };
-
-const append = (value: string) => {
-    if (formula.value === "0" && value === "0") {
-        return;
-    }
-
-    if (formula.value === "0" && value !== ".") {
-        formula.value = value;
-    } else {
-        formula.value += value;
-    }
-
-    setTimeout(scrollToRight, 1);
-};
-
-const scrollToRight = () => {
-    if (field.value) {
-        field.value.scrollLeft = field.value.scrollWidth;
-    }
-};
-
-const scrollToLeft = () => {
-    if (field.value) {
-        field.value.scrollLeft = 0;
-    }
-};
-
-const operator = (op: string) => {
-    formula.value += op;
-};
-
-const equal = () => {
-    if (mode.value === "client") {
-        try {
-            formula.value = eval(formula.value);
-        } catch (e) {
-            formula.value = ":(";
-        }
-    } else {
-        // TODO: Send the formula to the server and receive the result
-    }
-
-    if (field.value) {
-        setTimeout(scrollToLeft, 1);
-    }
-};
-
-const send = () => {};
 
 onMounted(async () => {
     document.addEventListener("gesturestart", function (e) {
