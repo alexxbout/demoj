9ba806786029a11205912a72d64af6f480d05198
alexxbout <boutinaud.pro@gmail.com>
2024-04-04 19:58:53 +0200

    updated const.py

--- module/scripts/const.py
+++ module/scripts/const.py
@@ -1,3 +1,11 @@
-IP_TERMINAL = "192.168.64.100"
-IP_NETWORK = "192.168.64.101"
-IP_SERVER = "192.168.64.102"
+# IP ADDRESS
+IP_TERMINAL = "10.3.141.2"
+IP_NETWORK = "10.3.141.1"
+IP_SERVER = "10.3.141.3"
+
+# COMMANDS
+RESTART_CMD = ["sudo", "reboot"]
+STOP_CMD = ["sudo", "shutdown", "-h", "now"]
+STRESS_LVL_1_CMD = ["stress", "-c", "1", "-i", "1", "-m", "1", "--timeout", "10s"]
+STRESS_LVL_2_CMD = ["stress", "-c", "2", "-i", "2", "-m", "2", "--timeout", "10s"]
+STRESS_LVL_3_CMD = ["stress", "-c", "4", "-i", "4", "-m", "4", "--timeout", "10s"]
