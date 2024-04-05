f0f6170060122ed96549e9288a9d9c19607fd288
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2024-04-05 19:22:07 +0200

    fixed main

--- module/scripts/app.py
+++ module/scripts/app.py
@@ -4,17 +4,27 @@ from zocket import socket_routine
 from server import server_routine
 import time
 
+def termination():
+    if socket_proc.is_alive():
+        socket_proc.terminate()
+    if server_proc.is_alive():
+        server_proc.terminate()
+    socket_proc.join()
+    server_proc.join()
+    socket_proc.close()
+    server_proc.close()
+
 if __name__ == "__main__":
     # leds = DemoLedsController()
-    cond = Condition()
-    socket_proc = Process(target=socket_routine, args=(cond,))
-    socket_proc.start()
     
-    server_proc = Process(target=server_routine)
-    server_proc.start()
     try:
-        while True:
+        while True: #restart the server if it crash
+            cond = Condition()
+            socket_proc = Process(target=socket_routine, args=(cond,))
+            server_proc = Process(target=server_routine)
             # leds.loading(255, 0, 0)
+            socket_proc.start()
+            server_proc.start()
             cond.acquire()
             cond.wait() # wait for socket connection
             # leds.loading_done()
@@ -22,7 +32,9 @@ if __name__ == "__main__":
             # leds.demoj()
             cond.wait() # wait for crash. or finish?
             cond.release()
+            termination()
     except KeyboardInterrupt:
         pass
-    # finally:
+    finally:
+        termination()
         # leds.close()
