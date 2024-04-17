dd73c711e05926fb5ddc5f9e925bf2e7911fe0ed
alexxbout <boutinaud.pro@gmail.com>
2024-04-17 13:35:43 +0200

    updated ui

--- scenarios-app/src/scenarios/AI.vue
+++ scenarios-app/src/scenarios/AI.vue
@@ -1,21 +1,134 @@
 <template>
-    <div class="flex flex-col w-screen h-screen p-5 overflow-x-hidden justify-stretch">
-        <div class="flex flex-col w-full h-max gap-y-5">
+    <div class="relative flex flex-col justify-between w-screen h-[100svh] overflow-hidden">
+        <div class="flex flex-col w-full p-5 h-max gap-y-5">
             <div class="flex items-center justify-between">
-                <span class="text-4xl font-semibold">IA</span>
+                <span class="text-4xl font-semibold">Intelligence Artificielle</span>
 
                 <!-- <div class="w-10 h-10">
                     <img v-show="loading" class="w-full h-full aspect-square" src="../assets/spinner.svg" alt="" />
                 </div> -->
             </div>
         </div>
+
+        <div @scroll="handleMessagesScroll" ref="messagesContainer" class="flex flex-col w-full h-full px-5 pt-10 pb-16 overflow-y-auto gap-y-10">
+            <!-- <div v-for="msg in messages" class="flex flex-row w-full h-full gap-y-5"> -->
+            <div v-for="msg in messages" class="flex w-full h-max gap-x-3">
+                <!-- Image -->
+                <svg v-if="msg.isResponse" class="w-6 h-6 fill-purple-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+                </svg>
+                <svg v-else class="w-6 h-6 fill-blue-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
+
+                <!-- Message -->
+                <div class="flex flex-col w-full gap-y-2 h-max">
+                    <span class="font-medium">{{ msg.isResponse ? "Assistant" : "Moi" }}</span>
+                    <span>{{ msg.text }}</span>
+                </div>
+            </div>
+        </div>
+        <!-- </div> -->
+
+        <div class="relative flex items-center justify-center w-full shadow-[0px_-40px_60px_20px_rgba(255,255,255,1)] px-2 pt-2 pb-5 h-max bg-white">
+            <label for="prompt" class="flex w-full p-2 px-3 border border-gray-300 rounded-full gap-x-2">
+                <input v-model="prompt" type="text" placeholder="Message..." id="prompt" name="prompt" class="w-full outline-none" />
+
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" :class="prompt.length == 0 ? 'fill-gray-300' : 'fill-gray-800'" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
+                </svg>
+            </label>
+
+            <div v-show="downArrowVisible" @click="handleGoToBottom" class="absolute flex items-center justify-center p-px bg-white border border-gray-300 rounded-full -inset-y-12 w-max h-max aspect-square">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 fill-gray-800" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
+                </svg>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+interface IMessage {
+    text: string;
+    isResponse: boolean;
+}
+
+const messages = ref<IMessage[]>([
+    {
+        text: "Hello, how can I help you?",
+        isResponse: true,
+    },
+    {
+        text: "I need help with my account",
+        isResponse: false,
+    },
+    {
+        text: "Sure, what do you need help with?",
+        isResponse: true,
+    },
+    {
+        text: "I need to change my password",
+        isResponse: false,
+    },
+    {
+        text: "I can help you with that, please provide me with your email address, and I will send you a link to reset your password, is that okay?",
+        isResponse: true,
+    },
+    {
+        text: "Thank you",
+        isResponse: false,
+    },
+    {
+        text: "You're welcome, have a nice day!",
+        isResponse: true,
+    },
+    {
+        text: "Goodbye",
+        isResponse: false,
+    },
+    {
+        text: "Goodbye, take care!",
+        isResponse: true,
+    },
+]);
+
+const prompt = ref<string>("");
+const messagesContainer = ref<HTMLElement | null>(null);
+const downArrowVisible = ref<boolean>(false);
+
+const handleGoToBottom = () => {
+    messagesContainer.value?.scrollTo({
+        top: messagesContainer.value.scrollHeight,
+        behavior: "smooth",
+    });
+
+    downArrowVisible.value = false;
+};
+
+const scrollToBottom = () => {
+    messagesContainer.value?.scrollTo({
+        top: messagesContainer.value.scrollHeight,
+        behavior: "instant",
+    });
+
+    downArrowVisible.value = false;
+};
+
+const handleMessagesScroll = () => {
+    if (messagesContainer.value) {
+        const { scrollTop, scrollHeight, clientHeight } = messagesContainer.value;
+
+        downArrowVisible.value = scrollTop + clientHeight < scrollHeight;
+    }
+};
 
 onMounted(() => {
     console.log("AI mounted");
+
+    scrollToBottom();
 });
 </script>
