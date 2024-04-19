39a2a9b8b50f013fe8bbf044238e58e8ff6f4819
alexxbout <boutinaud.pro@gmail.com>
2024-04-19 10:43:07 +0200

    working chat

--- scenarios-app/src/scenarios/AI.vue
+++ scenarios-app/src/scenarios/AI.vue
@@ -10,11 +10,10 @@
             </div>
         </div>
 
-        <div @scroll="handleMessagesScroll" ref="messagesContainer" class="flex flex-col w-full h-full px-5 pt-10 pb-16 overflow-y-auto gap-y-10">
-            <!-- <div v-for="msg in messages" class="flex flex-row w-full h-full gap-y-5"> -->
+        <div @scroll="handleScroll" ref="messagesContainer" class="flex flex-col w-full h-full px-5 pt-10 pb-16 overflow-y-auto gap-y-10">
             <div v-for="msg in messages" class="flex w-full h-max gap-x-3">
                 <!-- Image -->
-                <svg v-if="msg.role == 'assistant'" class="w-6 h-6 fill-purple-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <svg v-if="msg.role == 'assistant'" class="w-6 h-6 fill-purple-500" :class="loading ? 'animate-pulse' : ''" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
                 </svg>
                 <svg v-else class="w-6 h-6 fill-blue-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -29,14 +28,13 @@
                 </div>
             </div>
         </div>
-        <!-- </div> -->
 
         <div class="relative z-10 flex items-center justify-center w-full shadow-[0px_-30px_60px_20px_rgba(255,255,255,1)] px-2 pt-2 pb-5 h-max bg-white">
             <label for="prompt" class="flex w-full p-2 px-3 border border-gray-300 rounded-full gap-x-2">
-                <input v-model="prompt" type="text" placeholder="Message..." id="prompt" name="prompt" class="w-full outline-none" />
+                <input v-model="prompt" @keyup.enter="handleSend" type="text" placeholder="Message..." id="prompt" name="prompt" class="w-full outline-none" />
 
-                <button @click="handleSend" :disabled="prompt.length == 0">
-                    <svg xmlns="http://www.w3.org/2000/svg" :class="prompt.length == 0 ? 'fill-gray-300' : 'fill-gray-800'" class="w-8 h-8" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <button @click="handleSend" :disabled="prompt.length == 0 || loading">
+                    <svg xmlns="http://www.w3.org/2000/svg" :class="prompt.length == 0 || loading ? 'fill-gray-300' : 'fill-gray-800'" class="w-8 h-8" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
                     </svg>
                 </button>
@@ -77,7 +75,7 @@ const scrollToBottom = (mode: ScrollBehavior = "instant") => {
     downArrowVisible.value = false;
 };
 
-const handleMessagesScroll = () => {
+const handleScroll = () => {
     if (messagesContainer.value) {
         const { scrollTop, scrollHeight, clientHeight } = messagesContainer.value;
 
@@ -86,44 +84,41 @@ const handleMessagesScroll = () => {
 };
 
 const handleSend = async () => {
-    if (prompt.value.length == 0) return;
+    if (prompt.value.length == 0 || loading.value) return;
 
     loading.value = true;
 
+    await sendMessage(prompt.value, "user");
+
+    loading.value = false;
+};
+
+const sendMessage = async (message: string, role: "user" | "assistant") => {
     messages.value.push({
-        role: "user",
-        content: prompt.value,
+        role: role,
+        content: message,
     });
+
+    messages.value.push({
+        role: "assistant",
+        content: "",
+    });
+
+    prompt.value = "";
 
     setTimeout(scrollToBottom, 1);
 
     const requestData: OllamaApiRequest = {
-        model: "mistral",
-        prompt: prompt.value,
+        model: import.meta.env.VITE_OLLAMA_MODEL,
         messages: messages.value,
-        stream: false,
+        stream: true,
+        options: { num_predict: parseInt(import.meta.env.VITE_OLLAMA_MAX_TOKENS) },
     };
 
-    prompt.value = "";
-
-    const response = await new OllamaService("http://localhost:11434/api").generateResponse(requestData);
-
-    if (!Array.isArray(response) && response.message) {
-        loading.value = false;
-
-        messages.value.push(response.message);
-
-        setTimeout(() => scrollToBottom("smooth"), 1);
-
-        console.log(response);
-        
-        return;
-    }
+    await new OllamaService().getChatResponse(requestData, messages, scrollToBottom);
 };
 
 onMounted(async () => {
     console.log("AI mounted");
-
-    scrollToBottom();
 });
 </script>
--- scenarios-app/src/services/OllamaService.ts
+++ scenarios-app/src/services/OllamaService.ts
@@ -1,53 +1,84 @@
-import axios, { AxiosResponse } from 'axios';
+import { Ref, ref } from "vue";
 
 export interface OllamaApiResponse {
-  model: string;
-  created_at: string;
-  response: string;
-  done: boolean;
-  context: number[];
-  total_duration: number;
-  load_duration: number;
-  prompt_eval_count: number;
-  prompt_eval_duration: number;
-  eval_count: number;
-  eval_duration: number;
-  message?: OllamaMessage;
+    model: string;
+    created_at: string;
+    response: string;
+    done: boolean;
+    context: number[];
+    total_duration: number;
+    load_duration: number;
+    prompt_eval_count: number;
+    prompt_eval_duration: number;
+    eval_count: number;
+    eval_duration: number;
+    message?: OllamaMessage;
 }
 
 export interface OllamaApiRequest {
-  model: string;
-  prompt: string;
-  stream: boolean;
-  messages?: OllamaMessage[];
+    model: string;
+    prompt?: string; // only used when stream is false
+    stream: boolean;
+    messages?: OllamaMessage[];
+    options?: { num_predict: number };
 }
 
 export interface OllamaMessage {
-  role: "user" | "assistant";
-  content: string;
+    role: "user" | "assistant";
+    content: string;
 }
 
 class OllamaService {
-  private readonly baseUrl: string;
-  private readonly defaultOptions: Partial<OllamaApiRequest>;
+    private readonly baseUrl: string = import.meta.env.VITE_OLLAMA_API;
 
-  constructor(baseUrl: string, defaultOptions: Partial<OllamaApiRequest> = {}) {
-    this.baseUrl = baseUrl;
-    this.defaultOptions = defaultOptions;
-  }
+    async getChatResponse(requestData: OllamaApiRequest, messageRef: Ref<OllamaMessage[]>, scrollFunction: () => void) {
+        try {
+            const url = `${this.baseUrl}/chat`;
+            const headers = {
+                "Content-Type": "application/json",
+            };
 
-  async generateResponse(requestData: OllamaApiRequest): Promise<OllamaApiResponse | OllamaApiResponse[]> {
-    try {
-      const response = await axios.post<OllamaApiRequest, AxiosResponse<OllamaApiResponse | OllamaApiResponse[]>>(
-        `${this.baseUrl}/chat`,
-        requestData
-      );
-      return response.data;
-    } catch (error) {
-      // Gérer les erreurs ici
-      throw error;
+            const response = await fetch(url, {
+                method: "POST",
+                headers,
+                body: JSON.stringify(requestData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erreur lors de la requête: ${response.statusText}`);
+            }
+
+            const reader = response.body?.getReader();
+            if (!reader) {
+                throw new Error("Le corps de la réponse ne peut pas être lu.");
+            }
+
+            const lastMessage = messageRef.value[messageRef.value.length - 1];
+
+            const responseData = ref<OllamaMessage | null>(null);
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    break;
+                }
+
+                const chunk = new TextDecoder().decode(value);
+                const chunkData: OllamaApiResponse = JSON.parse(chunk);
+
+                if (chunkData.message) {
+                    responseData.value = chunkData.message;
+
+                    if (lastMessage) {
+                        lastMessage.content += responseData.value.content;
+
+                        scrollFunction();
+                    }
+                }
+            }
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 }
 
 export default OllamaService;
