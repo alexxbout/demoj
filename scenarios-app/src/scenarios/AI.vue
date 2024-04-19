88b9f70448950a64979294cff533d8211e515cfe
alexxbout <boutinaud.pro@gmail.com>
2024-04-19 11:21:32 +0200

    added nice animation for ai

--- scenarios-app/src/scenarios/AI.vue
+++ scenarios-app/src/scenarios/AI.vue
@@ -27,6 +27,13 @@
                     <span>{{ msg.content }}</span>
                 </div>
             </div>
+
+            <div v-show="messages.length == 0" class="flex flex-col items-center justify-center w-full h-full gap-y-5">
+                <svg class="w-20 h-20 fill-purple-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+                </svg>
+                <span ref="helloContainer" class="text-3xl font-medium transition-opacity duration-[700ms]">{{ hello }}</span>
+            </div>
         </div>
 
         <div class="relative z-10 flex items-center justify-center w-full shadow-[0px_-30px_60px_20px_rgba(255,255,255,1)] px-2 pt-2 pb-5 h-max bg-white">
@@ -57,8 +64,13 @@ const messages = ref<OllamaMessage[]>([]);
 
 const prompt = ref<string>("");
 const messagesContainer = ref<HTMLElement | null>(null);
+const helloContainer = ref<HTMLElement | null>(null);
 const downArrowVisible = ref<boolean>(false);
 const loading = ref<boolean>(false);
+
+// bonjour dans 20 langues
+const helloList: string[] = ["Bonjour", "Hello", "Hola", "Ciao", "Hallo", "Olá", "Saluton", "Hej", "Hei", "Ahoj", "Zdravo", "Dobrý den", "Dzień dobry", "Guten Tag", "Goedendag", "Γεια σας", "こんにちは", "안녕하세요", "你好", "नमस्ते"];
+const hello = ref<string>(helloList[0]);
 
 const handleGoToBottom = () => {
     scrollToBottom("smooth");
@@ -120,5 +132,32 @@ const sendMessage = async (message: string, role: "user" | "assistant") => {
 
 onMounted(async () => {
     console.log("AI mounted");
+
+    // Wait for 3 seconds
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    // Initialize index for helloList
+    let i = 0;
+
+    // Loop until the end of helloList
+    while (messages.value.length == 0) {
+        helloContainer.value?.classList.add("opacity-0");
+        await new Promise((resolve) => setTimeout(resolve, 700));
+
+        // Show the current hello message
+        hello.value = helloList[i + 1];
+
+        helloContainer.value?.classList.remove("opacity-0");
+
+        // Wait for 3 seconds
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        // Increment index for the next hello message
+        i++;
+
+        if (i == helloList.length - 1) {
+            i = -1;
+        }
+    }
 });
 </script>
