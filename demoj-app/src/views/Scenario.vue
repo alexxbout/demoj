5a2f7a86b33297a55fe059f112e2e5c19b9c742a
alexxbout <boutinaud.pro@gmail.com>
2024-04-24 22:09:40 +0200

    added scenario impact on system

--- demoj-app/src/views/Scenario.vue
+++ demoj-app/src/views/Scenario.vue
@@ -9,7 +9,7 @@
             </ion-toolbar>
         </ion-header>
         <ion-content class="ion-padding">
-            <div style="height: 100%; display: flex; flex-direction: column; justify-items: center; align-items: center; justify-content: space-between">
+            <div v-if="current" style="height: 100%; display: flex; flex-direction: column; justify-items: center; align-items: center; justify-content: space-between">
                 <div>
                     <ion-list :inset="true" style="margin: 0">
                         <ion-item v-if="mode == 'operator'">
@@ -25,8 +25,16 @@
                                 <ion-row>
                                     <ion-text>
                                         <h2>Description</h2>
-                                        <p>{{ description }}</p>
+                                        <p>{{ current.description }}</p>
                                     </ion-text>
+                                </ion-row>
+                                <ion-row class="">
+                                    <ion-col>
+                                        <ion-text>
+                                            <h2>Impact sur le système</h2>
+                                        </ion-text>
+                                        <ion-chip v-for="stress in current.stress" :key="stress" color="danger" class="ion-no-margin" style="margin-right: 10px;">{{ stress }}</ion-chip>
+                                    </ion-col>
                                 </ion-row>
                             </ion-grid>
                         </ion-item>
@@ -43,7 +51,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonBackButton, IonButton, IonButtons, IonChip, IonContent, IonGrid, IonHeader, IonItem, IonList, IonPage, IonRow, IonText, IonTitle, IonToggle, IonToolbar } from "@ionic/vue";
+import { IonBackButton, IonButton, IonButtons, IonChip, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonList, IonPage, IonRow, IonText, IonTitle, IonToggle, IonToolbar } from "@ionic/vue";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 
@@ -53,33 +61,49 @@ enum Scenario {
     Streaming = "streaming",
 }
 
+enum StressType {
+    CPU = "CPU",
+    MEMORY = "RAM",
+    NETWORK = "RÉSEAU",
+}
+
+interface ScenarioData {
+    scenario: Scenario;
+    description: string;
+    url: string;
+    stress: StressType[];
+}
+
 const mode = ref<"client" | "operator">(localStorage.getItem("mode") === "operator" ? "operator" : "client");
 const isDisabled = ref(true); // TODO: Update this value with socket data
 const canAccess = ref(mode.value === "operator" || !isDisabled);
-
-const description = ref("");
 
 const baseUrl = `http://${import.meta.env.VITE_IP_SERVER}:5000/app/`;
 
 const route = useRoute();
 
-const data: { scenario: Scenario; description: string; url: string }[] = [
+const data: ScenarioData[] = [
     {
         scenario: Scenario.Calculator,
         description: "Cette fonctionnalité permet aux utilisateurs d'effectuer une gamme étendue de calculs, depuis des opérations basiques jusqu'à des fonctions mathématiques avancées comme Fibonacci, factorielle et recherche de nombres premiers. Le tout peut être réalisé côté client ou côté serveur.",
         url: `${baseUrl}calculator`,
-    },
-    {
-        scenario: Scenario.AI,
-        description: "Cette fonctionnalité permet aux utilisateurs de communiquer avec une IA conversationnelle similaire à ChatGPT.",
-        url: `${baseUrl}ai`,
+        stress: [StressType.CPU, StressType.MEMORY],
     },
     {
         scenario: Scenario.Streaming,
         description: "Cette fonctionnalité permet aux utilisateurs de regarder des vidéos en streaming avec des fonctionnalités de contrôle similaires à Youtube.",
         url: `${baseUrl}streaming`,
+        stress: [StressType.NETWORK],
+    },
+    {
+        scenario: Scenario.AI,
+        description: "Cette fonctionnalité permet aux utilisateurs de communiquer avec une IA conversationnelle similaire à ChatGPT.",
+        url: `${baseUrl}ai`,
+        stress: [StressType.MEMORY, StressType.NETWORK],
     },
 ];
+
+const current = ref<ScenarioData | null>(null);
 
 const handleClick = () => {
     const scenario = data.find((d) => d.scenario === route.name);
@@ -94,7 +118,7 @@ onMounted(() => {
     const scenario = data.find((d) => d.scenario === route.name);
 
     if (scenario) {
-        description.value = scenario.description;
+        current.value = scenario;
     }
 });
 </script>
