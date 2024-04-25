6e5c91e011e2bd7babf865c0d2d5c57ebd80bb95
alexxbout <boutinaud.pro@gmail.com>
2024-04-25 15:57:49 +0200

    updated streaming

--- scenarios-app/src/scenarios/Streaming.vue
+++ scenarios-app/src/scenarios/Streaming.vue
@@ -1,22 +1,48 @@
 <template>
-    <div class="flex flex-col w-screen h-screen p-5 overflow-x-hidden justify-stretch">
-        <div class="flex flex-col w-full h-max gap-y-5">
-            <div class="flex items-center justify-between">
-                <span class="text-4xl font-semibold dark:text-white">Streaming</span>
+    <div class="flex flex-col w-screen h-[100svh] p-5 overflow-x-hidden justify-between">
+        <span class="text-4xl font-semibold dark:text-white">Streaming</span>
 
-                <div class="w-10 h-10">
-                    <img v-show="loading" class="w-full h-full aspect-square dark:invert" src="../assets/spinner.svg" alt="" />
-                </div>
-            </div>
+        <video class="rounded-xl shadow-2xl" controls muted>
+            <source v-if="url.length > 0" :src="url" type="video/mp4" />
+        </video>
+
+        <div class="flex flex-col w-full gap-y-5 h-max">
+            <select v-model="selected" class="w-full px-5 py-4 text-lg font-medium appearance-none h-max rounded-xl bg-slate-200" name="scenarioSelect" id="scenarioSelect" placeholder="Choisir une option...">
+                <option value="" disabled selected>Choisir une option...</option>
+                <option v-for="option in options" :value="option.value">{{ option.text }}</option>
+            </select>
+            <button @click="handleExecute" class="w-full py-4 text-lg font-medium text-white bg-blue-600 h-max rounded-xl disabled:grayscale" :disabled="disabled">Charger la vidéo</button>
         </div>
-
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
-const loading = ref(false);
+const base = `http://${import.meta.env.VITE_API_ENDPOINT}`;
+
+const options = ref<{ value: string; text: string }[]>([
+    {
+        value: "low",
+        text: "Vidéo basse qualité (480p)",
+    },
+    {
+        value: "medium",
+        text: "Vidéo haute qualité (4k)",
+    },
+]);
+
+const selected = ref<string>("");
+
+const disabled = computed(() => !selected.value);
+
+const url = ref<string>("");
+
+const handleExecute = async () => {
+    console.log("Loading video with quality: ", selected.value);
+
+    url.value = base + "/api/scenarios/streaming/" + selected.value;
+};
 
 onMounted(() => {
     console.log("Streaming mounted");
