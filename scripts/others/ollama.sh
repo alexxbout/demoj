69a38aa9b3c2ae96fcf6a13e41c3e26eb3743c6f
alexxbout <boutinaud.pro@gmail.com>
2024-04-25 16:43:04 +0200

    fixed bugs with ollama

--- scripts/others/ollama.sh
+++ scripts/others/ollama.sh
@@ -2,6 +2,7 @@
 # shellcheck shell=bash source=/dev/null disable=SC2154
 
 # ! NEED TO BE TESTED
+# ! Problem just after the install, ollama command not found, added systemctl start but need to be tested
 
 : '
 This script downloads and installs Ollama.
@@ -20,12 +21,14 @@ if [ "$user" != "server" ]; then
     exit 1
 fi
 
-model="llama3"
+model="phi"
 
 # Displaying initialization message
 echo "Initializing Ollama"
 
 curl -fsSL https://ollama.com/install.sh | sh >> "$log_file" 2>&1 || die "Failed to install Ollama"
+
+systemctl start ollama >> "$log_file" 2>&1 || die "Failed to start Ollama"
 
 ollama pull "$model" >> "$log_file" 2>&1 || die "Failed to pull $model"
 
