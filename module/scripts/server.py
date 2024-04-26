693cbd295d0fd3db486bf534212314bca5033065
alexxbout <boutinaud.pro@gmail.com>
2024-04-26 12:39:29 +0200

    added missing route for streaming

--- module/scripts/server.py
+++ module/scripts/server.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response
+from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response, send_from_directory
 from flask_cors import CORS
 from demojcompute import compute
 
-VIDEOS_FOLDER = "../videos"
+VIDEOS_FOLDER = "../../videos"
 HTTP_SERVER_PORT = 5000
 CURRENT_MODULE = "server"
 
@@ -20,6 +20,10 @@ def api():
 #################################################################
 # Scenarios app
 #################################################################
+
+@app.route('/videos/<path:filename>')
+def serve_video(filename):
+    return send_from_directory(app.static_folder + "/videos", filename)
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
