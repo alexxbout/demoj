a4d7107558d0b38c0ca398a823dafbd4cce99b17
alexxbout <boutinaud.pro@gmail.com>
2024-05-02 17:49:16 +0200

    temp

--- module/scripts/app.py
+++ module/scripts/app.py
@@ -5,14 +5,24 @@ import time
 from rpi_ws281x import Color
 
 if __name__ == "__main__":
-    leds = DemoLedsController()
+    try:
+        leds = DemoLedsController()
 
-    server_proc = Process(target=server_routine)
-    server_proc.start()
+        server_proc = Process(target=server_routine)
+        server_proc.start()
 
-    leds.loading(Color(0, 0, 255))
+        leds.loading(0, 0, 255)
 
-    # Fake loading time for network module since it does not have a loading process based on sockets
-    time.sleep(5)
+        # Fake loading time for network module since it does not have a loading process based on sockets
+        time.sleep(5)
 
-    leds.loading_done()
+        leds.loading_done()
+
+        leds.demoj()
+        
+        while True:
+            time.sleep(20)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        leds.close()
