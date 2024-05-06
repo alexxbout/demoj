9056386c02a8bb0290629edc3d7a24f208d8e7e9
alexxbout <boutinaud.pro@gmail.com>
2024-05-06 10:00:22 +0200

    removed server routine

--- module/scripts/app.py
+++ module/scripts/app.py
@@ -1,16 +1,10 @@
 from leds_process_control import DemoLedsController
-from multiprocessing import Process
-from server import server_routine
 import time
 
 if __name__ == "__main__":
     leds = DemoLedsController()
     try:
-
-        server_proc = Process(target=server_routine)        
-
         leds.loading(0, 0, 255)
-        server_proc.start()
 
         # Fake loading time for network module since it does not have a loading process based on sockets
         time.sleep(15)
@@ -18,8 +12,6 @@ if __name__ == "__main__":
         leds.loading_done()
 
         leds.demoj()
-        
-        server_proc.join()
     except KeyboardInterrupt:
         pass
     finally:
